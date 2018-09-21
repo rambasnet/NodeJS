bebcdca72329c8bd9ad099c1658b5a4074f7b70f
Ram Basnet <rambasnet@gmail.com>
2018-09-21 09:31:27 -0600

    added JSDemo/input1.js

--- JSDemo/input.js
+++ JSDemo/input.js
@@ -5,3 +5,8 @@ const readline = require('readline-sync');
 
 var name = readline.question('May I have your name? ');
 console.log('Hi there ' + name + '!');
+
+var num1 = readline.question("Enter a number between 1 and 20: ");
+console.log(typeof(num1));
+var num2 = Number(num1) ** 2;
+console.log("Squared of %s = %s", num1, num2);
