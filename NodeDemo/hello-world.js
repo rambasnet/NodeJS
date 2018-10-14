bd6a96db1bfa80353c7ff7d124fa3e1eb792593a
Ram Basnet <rambasnet@gmail.com>
2018-10-14 14:45:43 -0600

    Added Ch17 and 18 with Express demo

---
+++ NodeDemo/hello-world.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var dt = require('./datetime');
+
+const hostname = '127.0.0.1';
+const port = '9999';
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write('Hello from server...<br />');
+    response.write("Current date and time on the server: " + dt.myDateTime());
+    response.end();
+});
+
+server.listen(port, hostname, (err)=> {
+    if (err) {
+        console.log('error occured: ', err);
+        throw err;
+    }
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
