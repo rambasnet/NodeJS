72680dcb7e6b1cf54e12327ac1e130aa8227585e
Ram Basnet <rambasnet@gmail.com>
2018-10-14 20:14:19 -0600

    updated ExpressDemo and NodeDemo

--- ExpressDemo/app.js
+++ ExpressDemo/app.js
@@ -1,14 +1,13 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 const port = 9999;
 
-app.get('/', (req, res) => {
-   res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 var server = app.listen(port, () => {
-   var host = server.address().address;
-   var port = server.address().port;
-   console.log(`Example app listening at http://${host}:${port}`);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(`Example app listening at http://${host}:${port}`);
 });
-
--- NodeDemo/index.js
+++ NodeDemo/index.js
@@ -1,15 +1,26 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var fs = require("fs");
 
-http.createServer((req, res) => {
-    fs.readFile('index.html', (err, data) => {
-        if (err) {
-            res.writeHead(404, {'Content-Type':'text/html'});
-            res.write('404 Page Not found!');
-            return res.end()
-        }
-        res.writeHead(200, {'Content-Type':'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+const hostname = "127.0.0.1";
+const port = "9999";
+
+var server = http.createServer((req, res) => {
+  fs.readFile("index.html", (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write("404 Page Not found!");
+      return res.end();
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+});
+
+server.listen(port, hostname, err => {
+  if (err) {
+    console.log("error occured: ", err);
+    throw err;
+  }
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
