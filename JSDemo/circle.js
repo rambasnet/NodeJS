8a4d557b56ba17d9e2423ca02848582ae9b794cf
Ram Basnet <rambasnet@gmail.com>
2020-02-13 19:39:59 -0700

    updated stuff

--- JSDemo/circle.js
+++ JSDemo/circle.js
@@ -2,6 +2,7 @@
 This is module 2. Defines question, answer and
 circumference function
 */
+const assert = require('assert').strict;
 
 const { PI } = Math;
 
@@ -22,11 +23,20 @@ findArea = radius => PI * radius ** 2;
 
 module.exports = { findCircumference, findArea };
 
-//if (require.main == module) {
-console.log(`question: ${question}`);
-console.log(`answer: ${answer}`);
-var radius = 2;
-var circum = findCircumference(radius);
-console.log(`circumference = ${circum.toFixed(2)} unit`);
-console.log(`area = ${findArea(radius).toFixed(2)} sq. unit`);
-//}
+if (require.main == module) {
+  console.log(`question: ${question}`);
+  console.log(`answer: ${answer}`);
+  var radius = 2;
+  var circum = findCircumference(radius);
+  console.log(`circumference = ${circum.toFixed(2)} unit`);
+  console.log(`area = ${findArea(radius).toFixed(2)} sq. unit`);
+  test();
+}
+
+function test() {
+  var radius = 2;
+  var circum = findCircumference(radius);
+  console.log(circum.toFixed(2));
+  assert.strictEqual(circum.toFixed(2), '12.57', 'not equal');
+  console.log('all test cases passed!');
+}
--- JSDemo/moduleDemo.js
+++ JSDemo/moduleDemo.js
@@ -8,14 +8,19 @@ const math = require('./library/my_math.js');
 var question = "What is the meaning of Life, the Universe, and Everything?";
 var answer = 42;
 
+function test() {
+  console.log(`question: ${question}`);
+  console.log(`answer: ${answer}`);
 
-console.log(`question: ${question}`);
-console.log(`answer: ${answer}`);
+  console.log(circle)
+  console.log(math)
+  var area = circle.findArea(10);
+  console.log(`area = ${area.toFixed(2)} sq. unit`);
+  var circum = circle.findCircumference(10);
+  console.log(`circumference = ${circum.toFixed(2)} unit`);
+  console.log(`Third number in fibonacci seq = ${math.fib(3)}`);
+}
 
-console.log(circle)
-console.log(math)
-var area = circle.findArea(10);
-console.log(`area = ${area.toFixed(2)} sq. unit`);
-var circum = circle.findCircumference(10);
-console.log(`circumference = ${circum.toFixed(2)} unit`);
-console.log(`Third number in fibonacci seq = ${math.fib(3)}`);
+if (require.main == module) {
+    test();
+}
