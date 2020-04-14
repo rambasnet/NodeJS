b87c356af67388eaeda7f523593ef705ebac409a
Ram Basnet <rambasnet@gmail.com>
2020-04-14 16:17:13 -0600

    updated stuff

--- NodeDemo/hello-world.js
+++ NodeDemo/hello-world.js
@@ -6,8 +6,9 @@ const port = "9999";
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/html" });
-  response.write("Hello from server...<br />");
-  response.write("Current date and time on the server: " + dt.myDateTime());
+  response.write("<h1> Hello from server...</h1>");
+  response.write(`<p style="color: red"> Current date and time on the server: <strong> + 
+    ${dt.myDateTime()} + </strong></p>`);
   response.end();
 });
 
