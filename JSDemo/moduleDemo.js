c35a0f85eefd9caa67181639a73af9c09b488cfc
Ram Basnet <rambasnet@gmail.com>
2021-02-08 16:14:54 -0700

    update moduleDemo.js

--- JSDemo/moduleDemo.js
+++ JSDemo/moduleDemo.js
@@ -2,25 +2,26 @@
 This is module 1. Declares question, answer, and areaOfCircle function
 */
 
-const circle = require('./circle.js');
+//const circle = require('./circle');
+const {findCircumference, findArea} = require('./circle');
 const math = require('./library/my_math.js');
 
 var question = "What is the meaning of Life, the Universe, and Everything?";
 var answer = 42;
 
-function test() {
+function main() {
   console.log(`question: ${question}`);
   console.log(`answer: ${answer}`);
 
-  console.log(circle)
+  //console.log(circle)
   console.log(math)
-  var area = circle.findArea(10);
+  var area = findArea(10);
   console.log(`area = ${area.toFixed(2)} sq. unit`);
-  var circum = circle.findCircumference(10);
+  var circum = findCircumference(10);
   console.log(`circumference = ${circum.toFixed(2)} unit`);
   console.log(`Third number in fibonacci seq = ${math.fib(3)}`);
 }
 
 if (require.main === module) {
-    test();
+    main();
 }
